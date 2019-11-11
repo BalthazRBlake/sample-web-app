ba286783a2b89793f8aa4e2d4eeaa52854257bd3
FHHF <fabianhumbertohernandez@gmail.com>
2019-11-11 21:09:32 +0200

    1-Change authManaBuilder in SecurityConfig. 2-Add UserDetailsServiceImpl and UserDetailsImpl

---
+++ src/main/java/co/com/fhhf/deploymentfullapp/service/UserDetailsImpl.java
@@ -0,0 +1,58 @@
+package co.com.fhhf.deploymentfullapp.service;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ *
+ * @author FHHF
+ */
+public class UserDetailsImpl implements UserDetails{
+
+    private String name;
+    
+    public UserDetailsImpl(){
+    }
+
+    public UserDetailsImpl(String name){
+        this.name = name;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return "pass";
+    }
+
+    @Override
+    public String getUsername() {
+        return name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    
+}
